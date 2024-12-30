81f3cabaaf78187b1ad732d16d59c4040925febd
Mac <mac@Macs-MacBook-Pro.local>
2024-12-30 17:51:49 +0530

    update code

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
 // api/hello.js
 module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello, World! update' });
+    res.status(200).json({ message: 'Hello from the API!' });
   };
   
